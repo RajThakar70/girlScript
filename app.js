9db43378ebca31d0073d2b8458cde801b156c96d
RajThakar70 <rajthakar31@gmail.com>
2017-12-16 17:14:54 +0530

    mLabs added

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+try {
+  mongoose.connect("mongodb://SVN12:svn123@.mlab.com:21989/girlscript", {useMongoClient: true});
+  console.log("connected");
+} catch (e) {
+  console.log("error connecting database: " + e);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +26,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,7 +44,9 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development'
+    ? err
+    : {};
 
   // render the error page
   res.status(err.status || 500);
